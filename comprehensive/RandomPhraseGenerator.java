df3979f58c36ab34a52afa0553199d5d3325808a
ianvinson <60161085+ianvinson@users.noreply.github.com>
2020-04-19 13:17:54 -0700

    Add files via upload

--- comprehensive/RandomPhraseGenerator.java
+++ comprehensive/RandomPhraseGenerator.java
@@ -1,20 +1,118 @@
 package comprehensive;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Scanner;
 
-public class RandomPhraseGenerator
-{
+public class RandomPhraseGenerator {
 
-    Hashtable<String, NonTerminal> nonTerminals;
-    
-    private boolean inDefinition;
-    
-    public static void main (String[] args)
-    {
-        // TODO Auto-generated method stub
+	private static Hashtable<String, NonTerminal> nonTerminals;
 
-    }
-    
-    
+	private static boolean inDefinition;
+
+	private static boolean inTerminal;
+
+	private static int iterations;
+
+	public static void main(String[] args) throws FileNotFoundException {
+		iterations = Integer.parseInt(args[1]);
+		parseFile(args[0]);
+		sentenceCreator(iterations);
+	}
+
+	private static void parseFile(String filePath) throws FileNotFoundException {
+		File file = new File(filePath);
+		Scanner scn = new Scanner(file);
+		String currTag = null;
+
+		while (scn.hasNextLine()) {
+			String curr = scn.nextLine();
+			if (curr.charAt(0) == '{') {
+				inDefinition = true;
+				String line = scn.nextLine();
+				currTag = line;
+				nonTerminals.put(currTag, new NonTerminal(currTag));
+				curr = scn.nextLine();
+			}
+			
+			if (curr.charAt(0) == '}') {
+				inDefinition = false;
+			}
+			
+			if (inDefinition)
+			{
+				StringBuilder created = new StringBuilder();
+				Terminal original = null;
+				Terminal currentTerminal = null;
+				for (int i = 0; i < curr.length(); i++) 
+				{
+					if (curr.charAt(i) == '<') // if nonTerminal is encountered
+					{
+						inTerminal = true;
+						StringBuilder tag = new StringBuilder();
+
+						if (original == null) // Check original terminal and assign if it's the first terminal
+						{
+							original = new Terminal(created.toString());
+							currentTerminal = original;
+							nonTerminals.get(currTag).addTerminal(original);
+						} 
+						
+						else // if original is set, add to continuation
+						{
+							currentTerminal = new Terminal(created.toString());
+							original.addContinuation(currentTerminal);
+						}
+						
+						for (int j = i; curr.charAt(j) != '>'; j++) // get the tag of the nonterminal to reference and increment the outside loop
+						{
+							tag.append(curr.charAt(j));
+							i++;
+						}
+						tag.append(">"); //TODO Make sure that this increments to the correct spot
+						i++;
+						if (!nonTerminals.containsKey(tag.toString())) // Check if hashmap contains the tag, add it if it does not
+						{
+							nonTerminals.put(tag.toString(), new NonTerminal(tag.toString()));
+						}
+						
+						currentTerminal.setReferencedNonT(nonTerminals.get(tag.toString())); //set the Terminal's reference
+
+						created = new StringBuilder(); //clear the stringbuilder for the next terminal
+						inTerminal = false;
+					}
+
+					if (curr.charAt(i) != '<' && curr.charAt(i) != '>') // TODO Not sure if we need the conditional
+					{
+						created.append(curr.charAt(i));
+					}
+
+				}
+				
+				if (original == null) // No more non terminals encountered, Check original terminal and assign if it's
+									  // the first terminal. Add the terminal to the currTag list of terminals
+				{
+					original = new Terminal(created.toString());
+					nonTerminals.get(currTag).addTerminal(original);
+				} 
+				else
+				{
+					original.addContinuation(new Terminal(created.toString()));
+					nonTerminals.get(currTag).addTerminal(original);
+				}
+				
+			}
+
+		}
+
+	}
+	
+	private static void sentenceCreator(int iterations)
+	{
+		for (int i = 0; i < iterations; i++)
+			System.out.println(nonTerminals.get("<Start>").toString());
+	}
 
 }
--- comprehensive/Terminal.java
+++ comprehensive/Terminal.java
@@ -1,20 +1,75 @@
 package comprehensive;
 
+import java.util.ArrayList;
+
+/**
+ * A class that represents Terminals.This is any string of characters that is not a NonTerminal.
+ * A terminal always has a reference to a NonTerminal, even if there is no nonterminal following it, the referenced
+ * NonTerminal would be set to null. This is the base case and represents the end of the terminal.
+ * Whenever the NonTerminal is not null, continuations are possible.
+ * Continuations are terminals that follow after a NonTerminal (e.g. "The Dog" <verb> "at home")
+ * In the above example "at home" is the continuation.
+ * Continuations are stored in an ArrayList.
+ * 
+ * A terminal can have 3 cases:
+ * 
+ * 1. A terminal followed by a non terminal. (e.g. "The dog" <verb>). Continuations are possible
+ * 2. A terminal with the words set to an empty string followed by non terminal. (e.g. <verb>). Continuations are possible
+ * 3. A terminal followed by no nonterminal and the referenced NonTerminal is set to null. (e.g. "The Dog")
+ * Continuations are not possible.
+ * 
+ * @author Ian
+ *
+ */
 public class Terminal
 {
     String words;
     NonTerminal referencedNT;
+    ArrayList<Terminal> continuations;
     
-    Terminal ()
+    Terminal (String s)
     {
-    	
+    	words = s;
+    	referencedNT = null;
+    	continuations = new ArrayList<Terminal>();
     }
     
-	public String getString()
+	/**
+	 * Returns a string representation of the terminal and its continuations
+	 * @return - the string representation of the terminal and its continuations.
+	 */
+    public String getString()
     {
-        if (referencedNT == null)
-        	return words;
-		return words + referencedNT.getString();
+        if (referencedNT == null) //base case. 
+        	return words; //If there is no nonterminal following a terminal, there will be no continuation
+
+    	String total = words + referencedNT.getString(); //otherwise, get the terminal and its following non-terminal
+    	for (int i = 0; i < continuations.size(); i++) //add any continuations
+    	{
+    		total = total + continuations.get(i).getString();
+    	}
+    	return total;
     }
+	
+	/**
+	 * Adds to the array list that stores continuations terminals.
+	 * 
+	 * Continuation terminals happen if there is more terminals after a nonterminal
+	 * @param t
+	 */
+	public void addContinuation(Terminal t)
+	{
+		continuations.add(t);
+	}
+	
+	/**
+	 * sets the terminal's referenced non terminal. 
+	 * 
+	 * @param nt - the NonTerminal to reference
+	 */
+	public void setReferencedNonT(NonTerminal nt)
+	{
+		referencedNT = nt;
+	}
 
 }
